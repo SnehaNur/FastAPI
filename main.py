da4cfa6e9e7c11f16feb3f026535784aed65d59d
SnehaNur <snehanur1616@gmail.com>
2025-03-21 19:39:36 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+List = [
+    {'name':'Hafsa','id':'202020','dep':'development'},
+    {'name':'Hasib','id':'201120','dep':'development'},
+    {'name':'Rubaiya','id':'332020','dep':'development'},
+    {'name':'Rahim','id':'20202052','dep':'development'}
+]
+
 @app.get("/")
-def home():
-    return {"message": "FastAPI is working!"}
+async def info()-> list[dict]:
+    return List
